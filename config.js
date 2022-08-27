61f2e80364c8902e3f90cad19cd571df79dc9fa4
Thịnh <nd.thinh7898@gmail.com>
2022-08-27 21:58:36 +0700

    init file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Chào bạn Lan dễ thương ! 🖐🖐🖐',
+    introDesc: `T có cái này hay cực :v, nhấn vô để biết nhé !!!`,
+    btnIntro: 'OK',
+    title: 'c biết là t thích c đúng ko, c có muốn dành trung thu này đi chơi với t không ? ',
+    desc: 'trả lời thật lòng nha, ko đc out đó :> ',
+    btnYes: 'chán phèo, ở nhà vui hơn',
+    btnNo: '💖 Có, đương nhiên là thích đi chơi rồi 💖 ! ',
+    question:'Đùa tý thôi, vậy c thích đi chơi với t đúng ko 😝😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! ',
+    reply: 'đi đâu cũng thích, đi đâu cũng vui 😊😊',
+    mess: 'hẹn c dịp Trung thu nhé :3',
+    messDesc: '💬💬 inbox luôn để có thêm thông tin nha. 💬💬',
+    btnAccept: 'Ok lun :v',
+    messLink: 'https://www.facebook.com/ducthinh.nguyen.9400'
+}
